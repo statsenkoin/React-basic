abc931fd73d565e8ec5b306588fd6cb05c8312b0
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-12 22:47:17 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <img src="https://via.placeholder.com/960x240" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magni
+        commodi, non rerum eveniet ratione eligendi tempore architecto eaque
+        explicabo aspernatur deleniti distinctio praesentium in ab totam,
+        inventore facere error.
+      </p>
+    </main>
+  );
+};
