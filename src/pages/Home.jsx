289d6632c929a681f80035584d23f2d4b53172b2
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-13 18:19:47 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <main>
       <h1>Welcome</h1>
-      <img src="https://via.placeholder.com/960x240" alt="" />
+      <img src="https://via.placeholder.com/760x240" alt="" />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magni
         commodi, non rerum eveniet ratione eligendi tempore architecto eaque
