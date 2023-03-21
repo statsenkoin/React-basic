c1a103dc2eca57f2c2830d6db7d13fb2e95c01f1
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-21 18:59:51 +0200

    Update StatusFilter.js

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,41 @@
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
 
+import { useSelector } from 'react-redux';
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
+
+import { useDispatch } from 'react-redux';
+import { setStatusFilter } from 'redux/actions';
+
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
+
+  //   const filter = useSelector(state => state.filters.status);
+  const filter = useSelector(getStatusFilter);
+
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={handleFilterChange}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={handleFilterChange}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={handleFilterChange}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
