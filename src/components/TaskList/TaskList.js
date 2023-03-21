644e3a9e0570cc5feb9eadae3505cfeadbfd1558
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-21 19:00:13 +0200

    Update TaskList.js

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,35 @@
 import { Task } from 'components/Task/Task';
 import css from './TaskList.module.css';
 
+import { useSelector } from 'react-redux';
+import { statusFilters } from 'redux/constants';
+
+import { getStatusFilter, getTasks } from 'redux/selectors';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
 export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  //   const tasks = useSelector(state => state.tasks);
+  const tasks = useSelector(getTasks);
+  // Отримуємо значення фільтра із стану Redux
+  //   const statusFilter = useSelector(state => state.filters.status);
+  const statusFilter = useSelector(getStatusFilter);
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visibleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
