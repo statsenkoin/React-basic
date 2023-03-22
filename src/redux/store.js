a92ac92543a50607b84b63b8749899e5b386a7b3
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-22 12:34:47 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,10 @@
-import { legacy_createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { tasksReducer } from './taskSlice';
+import { filtersReducer } from './filtersSlice';
 
-import { rootReducer } from './reducer';
-
-// Початкове значення стану Redux для кореневого редюсера,
-// якщо не передати параметр preloadedState.
-// const initialState = {
-//   tasks: [
-//     { id: 0, text: 'Learn HTML and CSS', completed: true },
-//     { id: 1, text: 'Get good at JavaScript', completed: true },
-//     { id: 2, text: 'Master React', completed: false },
-//     { id: 3, text: 'Discover Redux', completed: false },
-//     { id: 4, text: 'Build amazing apps', completed: false },
-//   ],
-//   filters: {
-//     status: 'all',
-//   },
-// };
-
-// const rootReduces = (state = initialState, action) => {
-//   return state;
-// };
-
-const enhancer = devToolsEnhancer();
-
-export const store = legacy_createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
